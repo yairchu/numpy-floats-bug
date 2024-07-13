45507460ea7076169ed519a89b918df071d6c2b7
Yair Chuchem <yairchu@gmail.com>
2024-07-13 20:59:30 +0300

    add cdouble_square.s even though it is auto-generated for broader reproducibility (no need for numpy)

--- asm-repro/build.sh
+++ asm-repro/build.sh
@@ -2,7 +2,6 @@
 set -e
 
 cd "$(dirname $0)"
-python extract-func-asm.py
 as -arch arm64 -o asm-repro.o asm-repro.s
 ld -L$(xcrun --sdk macosx --show-sdk-path)/usr/lib -lSystem -o asm-repro asm-repro.o
 echo Success building asm-repro
